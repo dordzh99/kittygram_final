7a45ab163635c74eadddf62ace9b3f32f3ecc836
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-09-19 11:46:37 +0300

    kittygram_final review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost')
 
