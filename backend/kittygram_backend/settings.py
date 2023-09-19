e6ecab04667884025bc50bbf429c37228f056195
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-09-19 18:40:41 +0300

    kittygram_final review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,11 +1,15 @@
 import os
+
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost')
 
