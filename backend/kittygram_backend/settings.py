9186c94bf6c6dd8090543455430803eb98a90564
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-09-21 12:55:37 +0300

    kittygram_final review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost')
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
